314160fe73f99cd07b9268608e00f1c256c424f8
jonmackeyparty <jon@jonmackey.party>
2019-03-20 22:05:01 -0400

    add delete and edit functionality to addresses; refactor loan linkage in users/show to update when roommate is removed via address edit'

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -58,7 +58,61 @@ class ResidencesController < ApplicationController
   end
 
   patch '/residences/:id' do
+    @residence=Residence.find(params[:id])
+      if !params[:residence][:address].empty?
+        @residence.address=params[:residence][:address]
+      else
+        erb :"/residences/#{@residence.id}"
+      end
 
+      if !params[:residence][:deposit].empty?
+        @residence.deposit=params[:residence][:deposit]
+      else
+        erb :"/residences/#{@residence.id}"
+      end
+
+      if params[:residence][:primary_residence]=="true"
+        @residence.primary_residence=true
+      end
+
+      if !params[:landlord][:name].empty?
+        landlord=Landlord.find_by(:name => params[:landlord][:name])
+          if !landlord
+            @residence.landlord=Landlord.create(:name => params[:landlord][:name])
+          else
+            @residence.landlord=landlord
+          end
+      else
+        @residence.landlord=Landlord.find(params[:landlord][:landlord_id].first)
+      end
+
+      if params[:roommate][:roommate_ids]
+        params[:roommate][:roommate_ids].each do |id|
+          roommate2=Roommate.find(id)
+            if !@residence.roommates.include?(roommate2)
+              @residence.roommates << roommate2
+            end
+        end
+      end
+
+      if !params[:roommate][:name].empty?
+        roommate=Roommate.create(:name => params[:roommate][:name])
+        @residence.roommates << roommate
+      end
+
+      @residence.save
+      redirect to :"/residences/#{@residence.id}"
+  end
+
+  delete '/residences/:id' do
+    if logged_in?
+      @user=current_user
+    residence = Residence.find(params[:id])
+    residence.delete
+    redirect to "/users/#{@user.id}"
+    else
+      erb :'login'
+    end
   end
 
 end
